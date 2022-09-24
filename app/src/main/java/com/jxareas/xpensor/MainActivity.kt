b7641da78fd65b923786e5a6fff51f6423d18a88
jxareas <areasjx@gmail.com>
2022-09-23 21:29:09 -0600

    Refactor Gradle Build

--- app/src/main/java/com/jxareas/xpensor/MainActivity.kt
+++ app/src/main/java/com/jxareas/xpensor/MainActivity.kt
@@ -2,10 +2,16 @@ package com.jxareas.xpensor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jxareas.xpensor.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
