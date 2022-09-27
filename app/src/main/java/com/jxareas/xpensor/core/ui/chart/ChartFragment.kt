6261ceb6bf563efe43061d77432d4cd4cac16756
jxareas <areasjx@gmail.com>
2022-09-26 18:09:26 -0600

    Add Bottom Navigation

---
+++ app/src/main/java/com/jxareas/xpensor/core/ui/chart/ChartFragment.kt
@@ -0,0 +1,32 @@
+package com.jxareas.xpensor.core.ui.chart
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.jxareas.xpensor.R
+
+class ChartFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = ChartFragment()
+    }
+
+    private lateinit var viewModel: ChartViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        return inflater.inflate(R.layout.fragment_chart, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(ChartViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
