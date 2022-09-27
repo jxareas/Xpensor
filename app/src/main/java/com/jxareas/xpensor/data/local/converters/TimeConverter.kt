2dde7209ad4c3d2f9d88cde8fa268e37e0280b45
jxareas <areasjx@gmail.com>
2022-09-27 17:29:39 -0600

    Temp Fix Dark Theme

---
+++ app/src/main/java/com/jxareas/xpensor/data/local/converters/TimeConverter.kt
@@ -0,0 +1,18 @@
+package com.jxareas.xpensor.data.local.converters
+
+import androidx.room.TypeConverter
+import java.time.LocalTime
+
+class TimeConverter {
+
+    @TypeConverter
+    fun fromLocalTime(time: LocalTime): String {
+        return time.toString()
+    }
+
+    @TypeConverter
+    fun toLocalTime(data: String): LocalTime {
+        return LocalTime.parse(data)
+    }
+
+}
