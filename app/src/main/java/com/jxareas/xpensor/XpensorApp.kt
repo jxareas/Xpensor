39255de5348a0283d8a6f27bc1a1be9a989b01e6
jxareas <areasjx@gmail.com>
2022-10-15 21:18:15 -0600

    Set Preference Theme OnChangeListener

--- app/src/main/java/com/jxareas/xpensor/XpensorApp.kt
+++ app/src/main/java/com/jxareas/xpensor/XpensorApp.kt
@@ -1,7 +1,30 @@
 package com.jxareas.xpensor
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.PreferenceManager
+import com.jxareas.xpensor.utils.PreferenceUtils.THEME_DARK
+import com.jxareas.xpensor.utils.PreferenceUtils.THEME_DEFAULT
+import com.jxareas.xpensor.utils.PreferenceUtils.THEME_LIGHT
+import com.jxareas.xpensor.utils.PreferenceUtils.THEME_PREFERENCE_KEY
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class XpensorApp : Application()
+class XpensorApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        val selectedTheme = PreferenceManager.getDefaultSharedPreferences(applicationContext)
+            .getString(THEME_PREFERENCE_KEY, THEME_DEFAULT)
+
+        AppCompatDelegate.setDefaultNightMode(
+            when(selectedTheme) {
+                THEME_LIGHT -> AppCompatDelegate.MODE_NIGHT_NO
+                THEME_DARK -> AppCompatDelegate.MODE_NIGHT_YES
+                else -> AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+            }
+        )
+    }
+
+}
--- app/src/main/java/com/jxareas/xpensor/utils/ViewUtils.kt
+++ app/src/main/java/com/jxareas/xpensor/utils/ViewUtils.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.graphics.drawable.DrawableCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.DialogFragmentNavigator
@@ -13,6 +14,11 @@ import androidx.navigation.fragment.FragmentNavigator
 import androidx.navigation.fragment.findNavController
 import androidx.viewbinding.ViewBinding
 import com.jxareas.xpensor.utils.PreferenceUtils.MAIN_COLOR
+
+fun AppCompatDelegate.setNightMode(onSelectedTheme: () -> Int) =
+    AppCompatDelegate.setDefaultNightMode(
+        onSelectedTheme()
+    )
 
 fun ImageView.setTint(value: String?) {
     DrawableCompat.setTint(this.drawable, Color.parseColor(value ?: MAIN_COLOR))
