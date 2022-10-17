f0b87cf00177b4cf11cf0f7c787c877a5f4965ae
jxareas <areasjx@gmail.com>
2022-10-17 02:41:36 -0600

    Add Splash Screen

--- app/src/main/java/com/jxareas/xpensor/ui/main/MainActivity.kt
+++ app/src/main/java/com/jxareas/xpensor/ui/main/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
@@ -36,6 +37,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setupNavigation()
