1b5d3312d40944ea969499ea646b7db8dfe41ee4
jxareas <areasjx@gmail.com>
2022-11-08 01:16:54 -0600

    Add About Fragment Layout

---
+++ app/src/main/java/com/jxareas/xpensor/ui/about/AboutFragment.kt
@@ -0,0 +1,32 @@
+package com.jxareas.xpensor.ui.about
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.jxareas.xpensor.R
+
+class AboutFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = AboutFragment()
+    }
+
+    private lateinit var viewModel: AboutViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        return inflater.inflate(R.layout.fragment_about, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(AboutViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/jxareas/xpensor/ui/about/AboutViewModel.kt
@@ -0,0 +1,7 @@
+package com.jxareas.xpensor.ui.about
+
+import androidx.lifecycle.ViewModel
+
+class AboutViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
