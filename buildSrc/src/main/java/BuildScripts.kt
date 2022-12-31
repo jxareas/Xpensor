f93ef8d14ceb540a850194b836d51cdf0fde785f
Jon Areas <areasjx@gmail.com>
2022-12-31 02:34:03 -0600

    Add Hooks

--- buildSrc/src/main/java/BuildScripts.kt
+++ buildSrc/src/main/java/BuildScripts.kt
@@ -1,4 +1,5 @@
 object BuildScripts {
     const val KTLINT = "buildscripts/ktlint.gradle"
     const val VERSIONS = "buildscripts/versions.gradle"
+    const val HOOKS = "buildscripts/hooks.gradle"
 }
